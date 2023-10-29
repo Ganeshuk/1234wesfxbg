58afa94de29fd0081b32ba744bf1a7d591231278
Ganeshuk <manneganeshveerraju@gmail.com>
2023-10-29 07:06:11 +0000

    solution

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const cors = require("cors"); //import the cors package
+app.use(express.json());
+app.use(cors()); //use the
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "sql12.freesqldatabase.com",
+  password: "UGxYwwG72k",
+  user: "sql12655330",
+  database: "sql12655330",
+});
+
+app.listen(3001, () => {
+  console.log("ruuner");
+});
+app.get("/", (request, response) => {
+  db.query(`select * from ticket;`, (err, row) => {
+    if (err) {
+      response.send(err);
+    } else {
+      response.send(row);
+    }
+  });
+});
+
+app.post("/push/:id", (request, response) => {
+  const { id } = request.params;
+  db.query(`update ticket set book=1 where id=${id};`, (err, row) => {
+    if (err) {
+      response.send(err);
+    } else {
+      response.send("added");
+    }
+  });
+});
